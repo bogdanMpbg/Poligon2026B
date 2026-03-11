ad4500bcdf8a2333266ef0c93e06aabe6cf06b94
Ucenik <Ucenik@DESKTOP-USO4G6P>
2026-03-11 19:25:22 +0100

    v 0.2

---
+++ Poligon2026B/tacka.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poligon2026B
+{
+    internal class tacka
+    {
+        public double x;
+        public double y;
+        public double d()
+        {
+            return Math.Sqrt(x * x + y * y);
+        }
+        public tacka(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
