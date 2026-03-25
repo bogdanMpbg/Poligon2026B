fbdb8cf6aa7f8ad64d92f2a0344f4205f806e7d2
bogdanMpbg <bogdan.markovic@prvabeogim.edu.rs>
2026-03-25 19:38:33 +0100

    unos, stampa, snimi, ucitaj

--- Poligon2026B/Program.cs
+++ Poligon2026B/Program.cs
@@ -11,12 +11,22 @@ namespace Poligon2026B
         static void Main(string[] args)
         {
             // Bogdan Markovic
+
+            /*
             tacka A = new tacka(2, 3);
             tacka B = new tacka(4, 5);
             tacka C = new tacka(6, 7);
             tacka D = new tacka(8, 9);
             vektor AB = new vektor(A, B);
             AB.stampaj();
+            */
+
+            poligon P = poligon.unos();
+            P.stampa();
+            P.snimi();
+            poligon P2 = poligon.ucitaj();
+            P2.stampa();
+
             Console.ReadKey();
         }
     }
---
+++ Poligon2026B/poligon.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Poligon2026B
+{
+    internal class poligon
+    {
+        int br_temena;
+        tacka[] teme;
+
+        public poligon(int n)
+        {
+            br_temena = n;
+            teme = new tacka[n];
+        }
+
+        public static poligon unos()
+        {
+            Console.Write("Unesite broj temena: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            poligon rez = new poligon(n);
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Teme({i+1}).x = ");
+                double x = Convert.ToDouble(Console.ReadLine());
+                Console.Write($"Teme({i+1}).y = ");
+                double y = Convert.ToDouble(Console.ReadLine());
+                rez.teme[i] = new tacka(x, y);
+            }
+            return rez;
+        }
+
+        public void stampa()
+        {
+            Console.WriteLine($"Poligon sa {br_temena} tacaka");
+            for (int i = 0; i < br_temena; i++)
+            {
+                Console.WriteLine($"x={teme[i].x}, y={teme[i].y}");
+            }
+        }
+
+        public static poligon ucitaj()
+        {
+            StreamReader ulaz = new StreamReader("poligon.txt");
+            int n = Convert.ToInt32(ulaz.ReadLine());
+            poligon rez = new poligon(n);
+            for (int i = 0; i < n; i++)
+            {
+                double x = Convert.ToDouble(ulaz.ReadLine());
+                double y = Convert.ToDouble(ulaz.ReadLine());
+                rez.teme[i] = new tacka(x, y);
+            }
+            ulaz.Close();
+            return rez;
+        }
+
+        public void snimi()
+        {
+            StreamWriter izlaz = new StreamWriter("poligon.txt");
+            izlaz.WriteLine($"{br_temena}");
+            for (int i = 0; i < br_temena; i++)
+            {
+                izlaz.WriteLine($"{teme[i].x}");
+                izlaz.WriteLine($"{teme[i].y}");
+            }
+            izlaz.Close();
+        }
+    }
+}
